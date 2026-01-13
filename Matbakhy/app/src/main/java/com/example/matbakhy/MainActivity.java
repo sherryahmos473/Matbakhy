e6ef405ca132074cb9cce6b6f41fd4afc9444d83
sherryahmos473 <sherry.ahmos@gmail.com>
2026-01-13 20:39:47 +0200

    create application

---
+++ Matbakhy/app/src/main/java/com/example/matbakhy/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.matbakhy;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this,AuthActivity.class);
+                startActivity(intent);
+                finish();
+            }
+
+        },2000);
+    }
+}
